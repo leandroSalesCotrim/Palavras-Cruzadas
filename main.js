3a58cb0967d83c8f673248cf36b0680fe4ad9bae
leandroSalesCotrim <deadpoolleandro@hotmail.com>
2021-11-05 23:59:13 -0300

    html e js corrigidos

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function validar_2() {
     var input2 = inp_2.value;
  
 
-    if (input2.toLowerCase() == 'pc') {
+    if (input2.toLowerCase() == 'ula') {
         alert('Resposta Certa')
         $('.res2').css('opacity', '1')
         $('.res2').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -34,7 +34,7 @@ function validar_3() {
     var input3 = inp_3.value;
  
 
-    if (input3.toLowerCase() == 'ula') {
+    if (input3.toLowerCase() == 'registradores') {
         alert('Resposta Certa')
         $('.res3').css('opacity', '1')
         $('.res3').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -50,7 +50,7 @@ function validar_4() {
     var input4 = inp_4.value;
  
 
-    if (input4.toLowerCase() == 'arduino' || input4.toLowerCase() == 'arduíno') {
+    if (input4.toLowerCase() == 'ram' ) {
         alert('Resposta Certa')
         $('.res4').css('opacity', '1')
         $('.res4').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -66,7 +66,7 @@ function validar_5() {
     var input5 = inp_5.value;
  
 
-    if (input5.toLowerCase() == 'windows') {
+    if (input5.toLowerCase() == 'rom') {
         alert('Resposta Certa')
         $('.res5').css('opacity', '1')
         $('.res5').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -82,7 +82,7 @@ function validar_6() {
     var input6 = inp_6.value;
  
 
-    if (input6 == 'ubuntu') {
+    if (input6.toLowerCase() == 'eprom') {
         alert('Resposta Certa')
         $('.res6').css('opacity', '1')
         $('.res6').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -98,7 +98,7 @@ function validar_7() {
     var input7 = inp_7.value;
  
 
-    if (input7.toLowerCase() == 'ram') {
+    if (input7.toLowerCase() == 'flash') {
         alert('Resposta Certa')
         $('.res7').css('opacity', '1')
         $('.res7').css('backgroundColor', 'rgb(85, 224, 125)')
@@ -114,10 +114,142 @@ function validar_8() {
     var input8 = inp_8.value;
  
 
-    if (input8 == 'cmd') {
-        alert('Resposta Certa, você acabou o questionário!!')
+    if (input8 == 'memória de massa' || input8.toLowerCase() == 'memoria de massa') {
+        alert('Resposta Certa')
         $('.res8').css('opacity', '1')
         $('.res8').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_8').css('display', 'none')
+        $('#pergunta_9').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+function validar_9() {
+    var input9 = inp_9.value;
+ 
+
+    if (input9.toLowerCase() == 'dma') {
+        alert('Resposta Certa')
+        $('.res9').css('opacity', '1')
+        $('.res9').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_9').css('display', 'none')
+        $('#pergunta_10').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+function validar_10() {
+    var input10 = inp_10.value;
+ 
+
+    if (input10.toLowerCase() == 'cs') {
+        alert('Resposta Certa')
+        $('.res10').css('opacity', '1')
+        $('.res10').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_10').css('display', 'none')
+        $('#pergunta_11').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+function validar_11() {
+    var input11 = inp_11.value;
+ 
+
+    if (input11.toLowerCase() == 'address bus') {
+        alert('Resposta Certa')
+        $('.res11').css('opacity', '1')
+        $('.res11').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_11').css('display', 'none')
+        $('#pergunta_12').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+function validar_12() {
+    var input12 = inp_12.value;
+ 
+
+    if (input12.toLowerCase() == 'data bus') {
+        alert('Resposta Certa')
+        $('.res12').css('opacity', '1')
+        $('.res12').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_12').css('display', 'none')
+        $('#pergunta_13').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+function validar_13() {
+    var input13 = inp_13.value;
+ 
+
+    if (input13.toLowerCase() == 'i5') {
+        alert('Resposta Certa')
+        $('.res13').css('opacity', '1')
+        $('.res13').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_13').css('display', 'none')
+        $('#pergunta_14').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+
+function validar_14() {
+    var input14 = inp_14.value;
+ 
+
+    if (input14.toLowerCase() == 'i7') {
+        alert('Resposta Certa')
+        $('.res14').css('opacity', '1')
+        $('.res14').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_14').css('display', 'none')
+        $('#pergunta_15').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+
+function validar_15() {
+    var input15 = inp_15.value;
+ 
+
+    if (input15.toLowerCase() == 'dual core') {
+        alert('Resposta Certa')
+        $('.res15').css('opacity', '1')
+        $('.res15').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_15').css('display', 'none')
+        $('#pergunta_16').css('display', 'block')
+    }
+
+    else {
+        alert('Resposta Errada')
+    }
+}
+
+function validar_16() {
+    var input16 = inp_16.value;
+ 
+
+    if (input16.toLowerCase() == 'quad core') {
+        alert('Resposta Certa, você acabou o questionário!!')
+        $('.res16').css('opacity', '1')
+        $('.res16').css('backgroundColor', 'rgb(85, 224, 125)')
+        $('#pergunta_16').css('display', 'none')
         $('#resetar').css('display', 'block')
     }
 
@@ -125,6 +257,11 @@ function validar_8() {
         alert('Resposta Errada')
     }
 }
+
 function recomecar(){
     window.location.reload();
 }
+
+
+
+
